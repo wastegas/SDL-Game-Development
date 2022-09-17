c974888ac6a3b545bb437c2e3afb07e77e5e54eb
Ronnie Baron <wastegas@gmail.com>
2022-09-17 13:26:29 -1000

    use game class

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,24 @@
-#include <SDL.h>
-#include <iostream>
+#include "game.h"
 #include <config.h>
 
-SDL_Window*   g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
-
-bool init(const char* title, int xpos, int ypos, int height, int width,
-	  int flags)
-{
-  // Initialize SDL
-  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-    {
-      // Init succeeded create window
-      g_pWindow = SDL_CreateWindow(title,
-				   xpos,
-				   ypos,
-				   height, width,
-				   flags);
-      // Window created, create renderer
-      if (g_pWindow != 0)
-	{
-	  g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-	}
-      else
-	{
-	  return false;
-	}
-    }
-  else
-    {
-      return false; // something went wrong
-    }
-  return true;
-}
-
-
-void render()
-{
-  // set to black
-  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-  // clear the window
-  SDL_RenderClear(g_pRenderer);
-
-  std::cout << "drawing window" << std::endl;
-  // Show the window
-  SDL_RenderPresent(g_pRenderer);
-}  
+Game* g_game = 0;
 
 int main(int argc, char **argv)
 {
-  if(init(PACKAGE_STRING, SDL_WINDOWPOS_CENTERED,
-	  SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+  g_game = new Game();
+  
+  g_game->init(PACKAGE_STRING, SDL_WINDOWPOS_CENTERED,
+	       SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+  
+  while(g_game->running())
     {
-      g_bRunning = true;
-    }
-  else
-    {
-      return -1;
+      g_game->handleEvents();
+      g_game->update();
+      g_game->render();
     }
 
-  // delay before quitting
-  //  SDL_Delay(5000);
-
-  // clean up SDL
-  SDL_Quit();
+  g_game->clean();
+      
 
   return 0;
 }
