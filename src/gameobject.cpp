715884b9a06f3884eeae3942afff0ea3df1132c7
Ronnie Baron <wastegas@gmail.com>
2022-09-21 15:55:59 -1000

    convert to derived class

--- src/gameobject.cpp
+++ src/gameobject.cpp
@@ -1,19 +1,6 @@
 #include "gameobject.h"
 #include <config.h>
 
-void GameObject::load(int x, int y, int width, int height,
-		      std::string textureID)
-{
-  m_x = x;
-  m_y = y;
-  m_width = width;
-  m_height = height;
-  m_textureID = textureID;
-
-  m_currentRow = 1;
-  m_currentFrame = 1;
-}
-
 void GameObject::draw(SDL_Renderer* pRenderer)
 {
   TextureManager::Instance()->drawFrame(m_textureID, m_x, m_y,
--- src/gameobject.h
+++ src/gameobject.h
@@ -4,27 +4,19 @@
 #include <SDL.h>
 #include <iostream>
 #include <string>
-#include "texturemanager.h"
+//#include "texturemanager.h"
+#include "loaderparams.h"
 
 class GameObject
 {
  public:
-  virtual void load(int, int, int, int, std::string);
-  virtual void draw(SDL_Renderer*);
-  virtual void update();
-  virtual void clean() { std::cout << "gameobject clean" << std::endl; }
+  virtual void draw() = 0;
+  virtual void update() = 0;
+  virtual void clean() = 0;
 
  protected:
-  std::string m_textureID;
-
-  int m_currentFrame;
-  int m_currentRow;
-  
-  int m_x;
-  int m_y;
-
-  int m_width;
-  int m_height;
+  GameObject(const LoaderParams* pParams){}
+  virtual ~GameObject() {}
 };
 
 #endif // __GAMEOBECT_H__
