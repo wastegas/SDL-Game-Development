1da3fb46bad7a2fdd8e417e83ae1860c228a2cb8
Ronnie Baron <wastegas@gmail.com>
2022-09-21 16:01:24 -1000

    adjust call to functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,28 @@
 #include "game.h"
 #include <config.h>
 
-Game* g_game = 0;
-
 int main(int argc, char **argv)
 {
-  g_game = new Game();
+
   
-  g_game->init(PACKAGE_STRING, SDL_WINDOWPOS_CENTERED,
-	       SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-  
-  while(g_game->running())
+  if(TheGame::Instance()->init(PACKAGE_STRING, SDL_WINDOWPOS_CENTERED,
+			       SDL_WINDOWPOS_CENTERED,
+			       640, 480, false))
+    {  
+      while(TheGame::Instance()->running())
+	{
+	  TheGame::Instance()->handleEvents();
+	  TheGame::Instance()->update();
+	  TheGame::Instance()->render();
+	  SDL_Delay(10);
+	}
+    }
+  else
     {
-      g_game->handleEvents();
-      g_game->update();
-      g_game->render();
-      SDL_Delay(10);
+      return -1;
     }
 
-  g_game->clean();
+  TheGame::Instance()->clean();
       
 
   return 0;
