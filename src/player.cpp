cab350d1aec540f2e02599016c001eb83b5185f4
Ronnie Baron <wastegas@gmail.com>
2022-09-22 09:35:08 -1000

    use acceleration in our update function

--- src/player.cpp
+++ src/player.cpp
@@ -14,7 +14,7 @@ void Player::update()
 {
   //m_position.setX(m_position.getX() - 1);
   m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
-  m_velocity.setX(1);
+  m_acceleration.setX(1);
   SDLGameObject::update();
 }
 
