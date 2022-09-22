240c426167efe6895e06e26e3f2a0aef80453221
Ronnie Baron <wastegas@gmail.com>
2022-09-22 09:57:09 -1000

    use FPS for delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "game.h"
+#include <cstdint>
 #include <config.h>
+
+const int FPS = 60; // frames per sec
+const int DELAY_TIME = 1000.0f / FPS;
 
 int main(int argc, char **argv)
 {
+  uint32_t frameStart, frameTime;
 
   
   if(TheGame::Instance()->init(PACKAGE_STRING, SDL_WINDOWPOS_CENTERED,
@@ -11,10 +16,18 @@ int main(int argc, char **argv)
     {  
       while(TheGame::Instance()->running())
 	{
+	  frameStart = SDL_GetTicks();
+	  
 	  TheGame::Instance()->handleEvents();
 	  TheGame::Instance()->update();
 	  TheGame::Instance()->render();
-	  SDL_Delay(10);
+
+	  frameTime = SDL_GetTicks() - frameStart;
+
+	  if (frameTime < DELAY_TIME)
+	    {
+	      SDL_Delay((int) (DELAY_TIME - frameTime));
+	    }
 	}
     }
   else
