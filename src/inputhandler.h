5577e5d3dfcd24a782dffb8075f15c25a8d01fa0
Ronnie Baron <wastegas@gmail.com>
2022-09-22 17:00:01 -1000

    fix typo

--- src/inputhandler.h
+++ src/inputhandler.h
@@ -2,10 +2,17 @@
 #define __INPUTHANDLER_H__
 
 #include <SDL.h>
+#include <vector>
+#include <iostream>
+
 
 class InputHandler
 {
  public:
+  void initialiseJoysticks();
+  bool joysticksInitialised() { return m_bJoysticksInitialised; }
+  
+  
   static InputHandler* Instance()
   {
     if (s_pInstance == 0)
@@ -20,9 +27,12 @@ class InputHandler
 
  private:
   InputHandler();
-  ~InputHandler() {}
-
   static InputHandler* s_pInstance;
+  
+  std::vector<SDL_Joystick*> m_joysticks;
+  bool m_bJoysticksInitialised;
+
+
 };
 
 typedef InputHandler TheInputHandler;
