8a50a18f85bfdc0aa206f42216d45dfe83a77a56
Ronnie Baron <wastegas@gmail.com>
2022-09-25 09:56:53 -1000

    add support for keyboard events

--- src/player.cpp
+++ src/player.cpp
@@ -67,4 +67,22 @@ void Player::handleInput()
     }
   Velocity2D * vec = TheInputHandler::Instance()->getMousePosition();
   m_velocity = (*vec - m_position) / 100;
+
+  // keyboard
+  if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_RIGHT))
+    {
+      m_velocity.setX(2);
+    }
+  if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_LEFT))
+    {
+      m_velocity.setX(-2);
+    }
+  if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_UP))
+    {
+      m_velocity.setY(-2);
+    }
+  if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_DOWN))
+    {
+      m_velocity.setY(2);
+    }
 }
