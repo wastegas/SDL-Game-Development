a6c5d150f4c49ac381b4b9e0b616525f39756c7f
Ronnie Baron <wastegas@gmail.com>
2022-10-02 14:22:00 -1000

    new finite state machine

---
+++ src/gamestatemachine.cpp
@@ -0,0 +1,42 @@
+#include "gamestatemachine.h"
+
+void GameStateMachine::pushState(GameState* pState)
+{
+  m_gameStates.push_back(pState);
+  m_gameStates.back()->onEnter();
+}
+
+void GameStateMachine::popState()
+{
+  if (m_gameStates.empty())
+    {
+      if (m_gameStates.back()->onExit())
+	{
+	  delete m_gameStates.back();
+	  m_gameStates.pop_back();
+	}
+    }
+}
+
+void GameStateMachine::changeState(GameState* pState)
+{
+  if (m_gameStates.empty())
+    {
+      if(m_gameStates.back()->getStateID() == pState->getStateID())
+	{
+	  return; // do nothing
+	}
+
+      if (m_gatemStates.back()->onExit())
+	{
+	  m_gameStates.pop_back();
+	}
+    }
+
+  // push back our new state
+  m_gameStates.push_back(pState);
+
+  // initialise it
+  m_gameStates.back()->onEnter();
+}
+
---
+++ src/gamestatemachine.h
@@ -0,0 +1,18 @@
+#ifndef __GAMESTATEMACHINE_H__
+#define __GAMESTATEMACHINE_H__
+
+#include "gamestate.h"
+#include <vector>
+
+class GameStateMachine
+{
+ public:
+  void pushState(GameState* pState);
+  void changeState(GameState* pState);
+  void popState();
+
+ private:
+  std::vector<GameState*> m_gameStates;
+};
+
+#endif // __GAMESTATEMACHINE_H__
